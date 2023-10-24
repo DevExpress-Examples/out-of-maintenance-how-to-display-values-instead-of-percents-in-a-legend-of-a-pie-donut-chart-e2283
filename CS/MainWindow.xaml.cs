e14760b1e4279d6a2829bb55d00a33c35f5f2b04
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-24 11:08:05 +0400

    Change example status to out of maintenance

--- CS/MainWindow.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace WpfApplication1 {
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
-    }
-}
